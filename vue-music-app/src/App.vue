0bad394f752db7723ed1e97f08708eb247bbe886
AngelinaTyrael <lukcycc@AngelinaTyraeldeMacBook-Pro.local>
2022-01-03 01:28:35 +0800

    :tada:

---
+++ vue-music-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header></Header>
+  <Tab></Tab>
+  <router-view></router-view>
+</template>
+
+<script>
+import Header from '@/components/Header/Header'
+import Tab from '@/components/Tab/Tab'
+
+export default {
+  components: {
+    Header,
+    Tab
+  }
+}
+</script>
---
+++ vue-music-app/src/views/Recommend.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>recommend</div>
+</template>
+
+<script>
+import { getRecommend } from '@/service/recommend'
+
+export default {
+  name: 'recommend',
+  async created () {
+    const result = await getRecommend()
+    console.log(result)
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
